a192a563eeb31dd152ad5f7fda3ea24b59ed9669
Jarel Sanders <jarelsanders1986@gmail.com>
2023-06-18 12:56:55 -0400

    updated signup page

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -6,80 +6,98 @@ declare var $: any;
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
-export class SignUpComponent implements OnInit{
+export class SignUpComponent implements OnInit {
   signupUsers: any[] = [];
-  signupObj:any = {
+  signupObj: any = {
     email: '',
     password: '',
-    repeatpassword: ''
-  }
+    repeatpassword: '',
+  };
 
   signupForm: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.minLength(6)]),
-    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(18) ]),
-    repeatPassword: new FormControl('',[Validators.required, Validators.minLength(8), Validators.maxLength(18) ])
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(18),
+    ]),
+    repeatPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(18),
+    ]),
   });
 
-
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {
-      // checks if both password and repeat password fields match
+    // Email input validation
+    // let email2 = <HTMLInputElement>document.getElementById('#email');
+    // email2.addEventListener('keyup',(e) =>{
+    //   alert('Hi')
+    // })
+    // https://www.youtube.com/watch?v=CE5Sdb45NXE
+    //www.youtube.com/watch?v=CE5Sdb45NXE
+    //www.youtube.com/watch?v=CE5Sdb45NXE
+    //www.youtube.com/watch?v=CE5Sdb45NXE
+    //www.youtube.com/watch?v=CE5Sdb45NXE
+    //www.youtube.com/watch?v=CE5Sdb45NXE
+    //www.youtube.com/watch?v=CE5Sdb45NXE
+    //www.youtube.com/watch?v=CE5Sdb45NXE
+
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    $('#email').on('keyup', function () {
+      if ($('#email').val() != emailReg) {
+        // alert('no')
+
+        $('#message2')
+          .html('Enter a valid email address')
+          .css('color', 'green');
+      } else $('#message2').html('Not Matching').css('color', 'red');
+    });
+
+    // checks if both password and repeat password fields match
     $('#password, #confirm_password').on('keyup', function () {
       if ($('#password').val() == $('#confirm_password').val()) {
         $('#message').html('Matching').css('color', 'green');
-      }else 
-        $('#message').html('Not Matching').css('color', 'red');
+      } else $('#message').html('Not Matching').css('color', 'red');
     });
 
-
-   
-
-
     // all input tags blue
-    $("input").css("color","blue")
+    $('input').css('color', 'blue');
   }
 
- 
-
   // onSignUp(){
-  signUp(){
-    let pwd = (<HTMLInputElement>document.getElementById('')).value;
-    let pwdRepeat = (<HTMLInputElement>document.getElementById('')).value;
+  signUp() {
+    let pwd: string = (<HTMLInputElement>document.getElementById('')).value;
+    let pwdRepeat: string = (<HTMLInputElement>document.getElementById(''))
+      .value;
 
-
-    this.signupUsers.push(this.signupObj);//stores user that trying to signup into signupUsers array (push current object into the array)
-    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers))//(store the array into local storage)
+    this.signupUsers.push(this.signupObj); //stores user that trying to signup into signupUsers array (push current object into the array)
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers)); //(store the array into local storage)
     //reinitilize signupObj
     this.signupObj = {
-      email: '',
-      password: '',
-      repeatpassword: ''
-    }
+      email: String,
+      password: String,
+      repeatpassword: String,
+    };
 
-
-        // https://www.geeksforgeeks.org/how-to-validate-email-id-in-jquery/
+    // https://www.geeksforgeeks.org/how-to-validate-email-id-in-jquery/
     // validates if email is an actual email
-    $('#email').on('keyup',function(){
-
+    $('#email').on('keyup', function () {
       if ($('#email').val() == '') {
-       // $('#email').prop('required',true);
-       // $("[name='email']").alert("required", true)
-      alert('test')
- 
+        // $('#email').prop('required',true);
+        // $("[name='email']").alert("required", true)
+        alert('test');
       }
-       
- 
- 
-     })
-     if ($('#email').val() == null) {
+    });
+    if ($('#email').val() == null) {
       // $('#email').prop('required',true);
       // $("[name='email']").alert("required", true)
-     alert('test')
-
-     }
+      alert('test');
+    }
   }
 }
 // https://www.youtube.com/watch?v=38JJ0hDQoos
