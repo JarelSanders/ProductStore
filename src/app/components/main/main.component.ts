ef4f029aad27faed4c8f34f0f8860e106ef19b6b
Jarel Sanders <jarelsanders1986@gmail.com>
2023-06-25 18:04:01 -0400

    made header static in main component. Now working on media scaling

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-main',
@@ -8,3 +10,29 @@ import { Component } from '@angular/core';
 export class MainComponent {
 
 }
+
+// scrolling navbar
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  const navbarHeight = 100;
+  const distanceFromTop = Math.abs(
+    document.body.getBoundingClientRect().top
+  );
+  if (distanceFromTop >= navbarHeight) navbar?.classList.add("fixed-top");
+  else navbar?.classList.remove("fixed-top");
+});
+
+// declare var $: any;
+
+// $(document).ready(function(){
+//    const navbar = document.querySelector(".navbar");
+//    const navbarHeight = 100;
+//    const distanceFromTop = Math.abs(
+//      document.body.getBoundingClientRect().top
+//    );
+//   ${'navbar'}.on('scroll', function (){
+//     if ($(distanceFromTop >= $(navbarHeight))
+//     $("navbar".classL))
+
+//   })
+// })
--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -6,8 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent {
-subtotal: number = 0.07
-tax: number = 0.07
-shipping: number = 0.15
+  subtotal: number = 0.07
+  tax: number = 0.07
+  shipping: number = 0.15
+  item_amount:number = 12.99
+  subtotal_before: number = 25.98
+  grand_total: number = 150.99
+  
 
+  remove() {
+    console.log('Hi');
+    alert("hello");
+    // (<HTMLElement>document.getElementsByName(''))
+  }
 }
+
+
